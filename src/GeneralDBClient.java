8b6bc9d7356bd122650a9989e842216c798c640a
Hades.Yang <windofthesky@aliyun.com>
2015-08-13 14:30:34 +0800

    Update GeneralDBClient.java
    Update the latest version of GeneralDBClient.

--- src/GeneralDBClient.java
+++ src/GeneralDBClient.java
@@ -1,20 +1,19 @@
 /**
- * @Copyright to Yonggang.Yang .
+ * @Copyright to Hades.Yang 2015~2020.
  * @ClassName: GeneralDBClient.
  * @Project: AutoShardingDBPlatform.
  * @Package: generaldbplatform.
  * @Description: The basic client of the auto sharding general database platform.
- * @Author: Yonggang.Yang 
- * @Version: V2.0
+ * @Author: Hades.Yang 
+ * @Version: V1.0
  * @Date: 2015-07-22
  * @History: 
  *    1.2015-07-21 First version of GeneralDBClient was written.
+ *    2.2015-08-13 Modify the constructor function add ConfigDBClient support.
  */
 
 //package name.
 package generaldbplatform;
-
-
 
 
 public class GeneralDBClient 
@@ -44,11 +43,20 @@ public class GeneralDBClient
     
 
     
-    
-    public GeneralDBClient(String ip, String cache_channel, String persist_channel)
+    /**
+     * @Title: PersistentLayerClient.
+     * @Description: the construct function of this PersistentLayerClient class.
+	 * @param serverinfo_0: the first redis server node ip & port information in the redis sentinel.
+	 * @param serverinfo_1: the second redis server node ip & port information in the redis sentinel.
+     * @param serverinfo_2: the third redis server node ip & port information in the redis sentinel.
+	 * @param cache_channel: the channel that subscribed by the CacheLayerClient.    
+	 * @param persist_channel: the channel that subscribed by the PersistentLayerClient.
+     * @return none.
+     */    
+    public GeneralDBClient(String serverinfo_0, String serverinfo_1, String serverinfo_2, String cache_channel, String persist_channel)
     {
-    	this.mCacheClient = new CacheLayerClient(ip, cache_channel);
-    	this.mPersistClient = new PersistentLayerClient(ip, persist_channel);
+    	this.mCacheClient = new CacheLayerClient(serverinfo_0, serverinfo_1, serverinfo_2, cache_channel);
+    	this.mPersistClient = new PersistentLayerClient(serverinfo_0, serverinfo_1, serverinfo_2, persist_channel);
     	
     	if((this.mCacheClient.CachaInitOK == true) && (this.mPersistClient.PersistInitOK == true))
     	{
