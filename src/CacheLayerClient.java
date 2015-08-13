15a7a33f9538d3bcf86220c0bc23182543114444
Hades.Yang <windofthesky@aliyun.com>
2015-08-13 14:28:58 +0800

    Update CacheLayerClient.java
    Update the latest version of CacheLayerClient.

--- src/CacheLayerClient.java
+++ src/CacheLayerClient.java
@@ -1,14 +1,15 @@
 /**
- * @Copyright to Yonggang.Yang .
+ * @Copyright to Hades.Yang 2015~2020.
  * @ClassName: CacheLayerClient.
  * @Project: AutoShardingDBPlatform.
  * @Package: generaldbplatform.
  * @Description: The basic client of the auto sharding cache layer.
- * @Author: Yonggang.Yang 
+ * @Author: Hades.Yang
  * @Version: V2.0
  * @Date: 2015-07-21
  * @History: 
  *    1.2015-07-21 First version of CacheLayerClient was written.
+ *    2.2015-08-13 Modify subscribe interface,add ConfigDBClient support. 
  */
 //package name.
 package generaldbplatform;
@@ -58,7 +59,13 @@ public class CacheLayerClient
      */
     private int SentinelPoolTimeout = 100000;
 	
-	/**
+    /**
+     * @FieldName: configdb.
+     * @Description: the ConfigServer database client.
+     */
+	private ConfigDBClient configdb;
+	
+    /**
      * @FieldName: CACHE_SERVER_NUM_OLD.
      * @Description: the new mod_number which is used to store/fetch value from the old HashMap.
      */
@@ -76,21 +83,33 @@ public class CacheLayerClient
      */
     protected Map<Integer, JedisSentinelPool> CacheShardingMap;  
     
-	/**
+    /**
      * @FieldName: CachaInitOK.
      * @Description: the boolean value which shows the cache database client initialize ok or not.
      */
     protected boolean CachaInitOK = false;
 	
+    /**
+     * @Title: ConfigDBInit.
+     * @Description: this function is used to initialize the configdb.
+     * @param serverinfo_0: the first redis server node ip & port information in the redis sentinel.
+     * @param serverinfo_1: the second redis server node ip & port information in the redis sentinel.
+     * @param serverinfo_2: the third redis server node ip & port information in the redis sentinel.
+     * @return none.
+     */
+	private void ConfigDBInit(String serverinfo_0, String serverinfo_1, String serverinfo_2)
+	{
+		this.configdb = new ConfigDBClient(serverinfo_0, serverinfo_1, serverinfo_2);
+	}
 
     /**
      * @Title: Subscriber.
      * @Description: the function which is used to subscribe one channel from redis server.
      * @param ip: the redis server ip address. 
-	 * @param channel: the specific channel which was subscribed by the application.
+     * @param channel: the specific channel which was subscribed by the application.
      * @return none.
      */
-	private void Subscriber(final String ip, final String channel)
+	private void Subscriber(final String channel)
 	{
 		final JedisPubSub jedisPubSub = new JedisPubSub() 
 		{
@@ -111,17 +130,32 @@ public class CacheLayerClient
 			public void run() 
 			{
 				//System.out.println("Start!!!"); 
-				try 
-				{
-					Jedis jedis = new Jedis(ip, 6379, 0);  //0 means no timeout.
-					jedis.subscribe(jedisPubSub, channel);
-					jedis.quit();
-					jedis.close();
-				} 
-				catch (Exception e) 
-				{
-				    //e.printStackTrace();
-				}
+				
+                Jedis jedisConnector = null;
+		        boolean borrowOrOprSuccess = true;
+		
+		        try 
+		        {
+			        jedisConnector = configdb.db_client.getResource();
+			        jedisConnector.subscribe(jedisPubSub, channel);
+		        }
+		        catch(JedisConnectionException e)
+		        {
+			        borrowOrOprSuccess = false;
+			        if(jedisConnector != null)
+			        {
+				        configdb.db_client.returnBrokenResource(jedisConnector);
+				        jedisConnector = null;
+			        }
+			        throw e;
+		        }
+		        finally
+		        {
+			        if(borrowOrOprSuccess && (jedisConnector!=null))
+			        {
+				        configdb.db_client.returnResource(jedisConnector);
+			        }
+		        }
 			}
 		}, "subscriberThread").start();
 	}
@@ -249,18 +283,20 @@ public class CacheLayerClient
     /**
      * @Title: CacheLayerClient.
      * @Description: the construct function of the CacheLayerClient class.
-     * @param ip: the redis server which is used to publish the cache database info.
-	 * @param channel: the channel that subscribed by the CacheLayerClient.
-     * @return 
+     * @param serverinfo_0: the first redis server node ip & port information in the redis sentinel.
+     * @param serverinfo_1: the second redis server node ip & port information in the redis sentinel.
+     * @param serverinfo_2: the third redis server node ip & port information in the redis sentinel.
+     * @param channel: the channel that subscribed by the CacheLayerClient.
+     * @return none.
      */
-    public CacheLayerClient(String ip, String channel)
+    public CacheLayerClient(String serverinfo_0, String serverinfo_1, String serverinfo_2, String channel)
     {
 		this.PoolConfig = new GenericObjectPoolConfig();
 		this.PoolConfig.setMaxIdle(25);
 		this.PoolConfig.setMaxTotal(250);
 		this.PoolConfig.setMaxWaitMillis(10000);
-	    
+		this.ConfigDBInit(serverinfo_0, serverinfo_1, serverinfo_2);
     	this.CacheShardingMap = new HashMap<Integer, JedisSentinelPool>();
-    	this.Subscriber(ip, channel);
+    	this.Subscriber(channel);
     }
 }
