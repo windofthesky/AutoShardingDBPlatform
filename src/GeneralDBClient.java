f47f988db3f8f8cb475a9acba5b5717c5db9d170
Hades.Yang <windofthesky@aliyun.com>
2015-08-14 09:35:43 +0800

    Update GeneralDBClient.java
    Modify the code style.

--- src/GeneralDBClient.java
+++ src/GeneralDBClient.java
@@ -18,21 +18,21 @@ package generaldbplatform;
 
 public class GeneralDBClient 
 {
-	/**
+    /**
      * @FieldName: mCacheClient.
      * @Description: the cache database client as a private member, just can be used by the GeneralDBClient itself.
      *               (when we use the client, we get the cache database real client according to the key).
      */
     private CacheLayerClient mCacheClient;
     
-	/**
+    /**
      * @FieldName: mPersistClient.
      * @Description: the persistent database client as a private member,just can be used by the GeneralDBClient itself.
      *               (when we use the client, we get the persistent database real client according to the key).
      */
     private PersistentLayerClient mPersistClient;
     
-	/**
+    /**
      * @FieldName: allClientOK.
      * @Description: a boolean value which indicates the states of the CacheClient & PersistClient.
      *               (when both ok, and this boolean value should be true).
@@ -46,11 +46,11 @@ public class GeneralDBClient
     /**
      * @Title: PersistentLayerClient.
      * @Description: the construct function of this PersistentLayerClient class.
-	 * @param serverinfo_0: the first redis server node ip & port information in the redis sentinel.
-	 * @param serverinfo_1: the second redis server node ip & port information in the redis sentinel.
+     * @param serverinfo_0: the first redis server node ip & port information in the redis sentinel.
+     * @param serverinfo_1: the second redis server node ip & port information in the redis sentinel.
      * @param serverinfo_2: the third redis server node ip & port information in the redis sentinel.
-	 * @param cache_channel: the channel that subscribed by the CacheLayerClient.    
-	 * @param persist_channel: the channel that subscribed by the PersistentLayerClient.
+     * @param cache_channel: the channel that subscribed by the CacheLayerClient.    
+     * @param persist_channel: the channel that subscribed by the PersistentLayerClient.
      * @return none.
      */    
     public GeneralDBClient(String serverinfo_0, String serverinfo_1, String serverinfo_2, String cache_channel, String persist_channel)
