3a8c98bb8b8005c13e2dc29202adce18646a912a
Hades.Yang <windofthesky@aliyun.com>
2016-03-30 15:37:23 +0800

    Update PersistentLayerClient.java
    Modify the format of this file.

--- src/PersistentLayerClient.java
+++ src/PersistentLayerClient.java
@@ -31,8 +31,6 @@ import com.mongodb.ServerAddress;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPubSub;
 
-
-
 public class PersistentLayerClient 
 {
     /**
@@ -105,58 +103,56 @@ public class PersistentLayerClient
      * @param channel: the specific channel which was subscribed by the application.
      * @return none.
      */
-	private void Subscriber(final String channel)
+    private void Subscriber(final String channel)
+    {
+ 	final JedisPubSub jedisPubSub = new JedisPubSub() 
 	{
-		final JedisPubSub jedisPubSub = new JedisPubSub() 
-		{
-			@Override	
-			public void onMessage(String channel, String message) 
-			{  
-		        //System.out.println("Input channel =="+channel + "----input message ==" + message); 
+	    @Override	
+	    public void onMessage(String channel, String message) 
+	    {  
+		//System.out.println("Input channel =="+channel + "----input message ==" + message); 
 		        
-		        if(channel.equals("PERSIST_AUTO_EXTERN"))
-		        {
-		        	PersistentShardMapInit(message);
-		        }
-		    }  
-		};
-		new Thread(new Runnable() 
+		if(channel.equals("PERSIST_AUTO_EXTERN"))
 		{
-			@Override
-			public void run() 
-			{
-				//System.out.println("Start!!!"); 
-				
+		    PersistentShardMapInit(message);
+		}
+	    }  
+	};
+	new Thread(new Runnable() 
+	{
+	    @Override
+	    public void run() 
+	    {
+		//System.out.println("Start!!!"); 
                 Jedis jedisConnector = null;
-		        boolean borrowOrOprSuccess = true;
-		
-		        try 
-		        {
-			        jedisConnector = configdb.db_client.getResource();
-			        jedisConnector.subscribe(jedisPubSub, channel);
-		        }
-		        catch(JedisConnectionException e)
-		        {
-			        borrowOrOprSuccess = false;
-			        if(jedisConnector != null)
-			        {
-				        configdb.db_client.returnBrokenResource(jedisConnector);
-				        jedisConnector = null;
-			        }
-			        throw e;
-		        }
-		        finally
-		        {
-			        if(borrowOrOprSuccess && (jedisConnector!=null))
-			        {
-				        configdb.db_client.returnResource(jedisConnector);
-			        }
-		        }
-			}
-		}, "subscriberThread").start();
-	}
-
+		boolean borrowOrOprSuccess = true;
 	
+		try 
+		{
+	            jedisConnector = configdb.db_client.getResource();
+	            jedisConnector.subscribe(jedisPubSub, channel);
+		}
+		catch(JedisConnectionException e)
+		{
+		    borrowOrOprSuccess = false;
+	            if(jedisConnector != null)
+		    {
+		        configdb.db_client.returnBrokenResource(jedisConnector);
+		        jedisConnector = null;
+	            }
+	            throw e;
+		}
+		finally
+		{
+	            if(borrowOrOprSuccess && (jedisConnector!=null))
+		    {
+		        configdb.db_client.returnResource(jedisConnector);
+		    }
+		}
+	    }
+	}, "subscriberThread").start();
+    }
+    
     /**
      * @Title: PersistentShardMapInit.
      * @Description: the function is used to initialize the PersistentShardingMap.(when the client got the info_message)
@@ -164,54 +160,54 @@ public class PersistentLayerClient
      *        the message should be like this:"1.0.0.1:27018_1.0.2.3:27017_1.0.0.5:27019;1.0.0.22:27017_1.10.2.3:27018_12.0.0.5:27019".
      * @return none.
      */
-	public void PersistentShardMapInit(String info_message)
+    public void PersistentShardMapInit(String info_message)
+    {
+        //the list consists of ServerAddress which records the MongoDB ip & port information.
+	List<ServerAddress> server_addresses = new ArrayList<ServerAddress>();
+	
+	//split the info_message like this:"1.0.0.1:27017_1.0.2.3:27018_1.0.0.5:27019",3 ip:port segments.
+	String[] arr_new_node = info_message.split(";");  //must split by ";".
+			
+	//Assign the old mod number to CACHE_SERVER_NUM_OLD.
+	this.PERSIST_SERVER_NUM_OLD = PersistShardingMap.size();
+				
+	for(int i = 0; i < arr_new_node.length; i++)
 	{
-		//the list consists of ServerAddress which records the MongoDB ip & port information.
-		List<ServerAddress> server_addresses = new ArrayList<ServerAddress>();
-		
-		//split the info_message like this:"1.0.0.1:27017_1.0.2.3:27018_1.0.0.5:27019",3 ip:port segments.
-		String[] arr_new_node = info_message.split(";");  //must split by ";".
-				
-		//Assign the old mod number to CACHE_SERVER_NUM_OLD.
-		this.PERSIST_SERVER_NUM_OLD = PersistShardingMap.size();
-				
-		for(int i = 0; i < arr_new_node.length; i++)
-		{
-			//clear the old address info.
-			server_addresses.clear();
+	    //clear the old address info.
+	    server_addresses.clear();
 			
-			//split the "1.0.0.1:27017_1.0.2.3:27018_1.0.0.5:27019" like:"1.0.0.1:27017".
-			String[] arr_ip_port = arr_new_node[i].split("_");
+	    //split the "1.0.0.1:27017_1.0.2.3:27018_1.0.0.5:27019" like:"1.0.0.1:27017".
+	    String[] arr_ip_port = arr_new_node[i].split("_");
 					
-			//split the "1.0.0.1:27017" like: ip->1.0.0.1, port->27017.
-			String ip0 = arr_ip_port[0].split(":")[0];
-			int port0 = Integer.parseInt(arr_ip_port[0].split(":")[1]);
-			String ip1 = arr_ip_port[1].split(":")[0];
-			int port1 = Integer.parseInt(arr_ip_port[1].split(":")[1]);
-			String ip2 = arr_ip_port[2].split(":")[0];
-			int port2 = Integer.parseInt(arr_ip_port[2].split(":")[1]);
+	    //split the "1.0.0.1:27017" like: ip->1.0.0.1, port->27017.
+	    String ip0 = arr_ip_port[0].split(":")[0];
+	    int port0 = Integer.parseInt(arr_ip_port[0].split(":")[1]);
+	    String ip1 = arr_ip_port[1].split(":")[0];
+	    int port1 = Integer.parseInt(arr_ip_port[1].split(":")[1]);
+	    String ip2 = arr_ip_port[2].split(":")[0];
+	    int port2 = Integer.parseInt(arr_ip_port[2].split(":")[1]);
 			
-			//construct the 3 MongoDB address instances.
-			ServerAddress address0 = new ServerAddress(ip0, port0);
-			ServerAddress address1 = new ServerAddress(ip1, port1);
-			ServerAddress address2 = new ServerAddress(ip2, port2);
-			server_addresses.add(address0);
-			server_addresses.add(address1);
-			server_addresses.add(address2);
+	    //construct the 3 MongoDB address instances.
+	    ServerAddress address0 = new ServerAddress(ip0, port0);
+	    ServerAddress address1 = new ServerAddress(ip1, port1);
+	    ServerAddress address2 = new ServerAddress(ip2, port2);
+	    server_addresses.add(address0);
+	    server_addresses.add(address1);
+	    server_addresses.add(address2);
 			
-			//create a new MongoClient instance and insert it into PersistShardingMap with the latest mod index.
-			mongoClient_temp = new MongoClient(server_addresses,this.mongoOption);
-			this.PersistShardingMap.put(CurrentPersistMapIndex, mongoClient_temp);
-			CurrentPersistMapIndex += 1;
-		}
-			
-		//check the sharding map size and assign the value to PERSIST_SERVER_NUM_NEW.
-		if(PersistShardingMap.size()>0)
-		{
-			this.PERSIST_SERVER_NUM_NEW = PersistShardingMap.size(); 
-			this.PersistInitOK = true;
-		}
+	    //create a new MongoClient instance and insert it into PersistShardingMap with the latest mod index.
+	    mongoClient_temp = new MongoClient(server_addresses,this.mongoOption);
+	    this.PersistShardingMap.put(CurrentPersistMapIndex, mongoClient_temp);
+	    CurrentPersistMapIndex += 1;
 	}
+			
+	//check the sharding map size and assign the value to PERSIST_SERVER_NUM_NEW.
+	if(PersistShardingMap.size()>0)
+        {
+	    this.PERSIST_SERVER_NUM_NEW = PersistShardingMap.size(); 
+	    this.PersistInitOK = true;
+	}
+    }
 
     /**
      * @Title: getOldShardedPersistClient.
@@ -219,10 +215,10 @@ public class PersistentLayerClient
      * @param key: int value which is indicates the value.
      * @return MongoClient:the MongoDB client which is used to access the MongoDB.
      */
-	public MongoClient getOldShardedPersistClient(int key)
-	{
-		return this.PersistShardingMap.get(key % this.PERSIST_SERVER_NUM_OLD);
-	}
+    public MongoClient getOldShardedPersistClient(int key)
+    {
+	return this.PersistShardingMap.get(key % this.PERSIST_SERVER_NUM_OLD);
+    }
 	
     /**
      * @Title: getNewShardedPersistClient.
@@ -230,10 +226,10 @@ public class PersistentLayerClient
      * @param key: int value which is indicates the value.
      * @return MongoClient:the MongoDB client which is used to access the MongoDB.
      */
-	public MongoClient getNewShardedPersistClient(int key)
-	{
-		return this.PersistShardingMap.get(key % this.PERSIST_SERVER_NUM_NEW);
-	}
+    public MongoClient getNewShardedPersistClient(int key)
+    {
+	return this.PersistShardingMap.get(key % this.PERSIST_SERVER_NUM_NEW);
+    }
 	
     /**
      * @Title: getOldShardedPersistClient.
@@ -241,10 +237,10 @@ public class PersistentLayerClient
      * @param key: long value which is indicates the value.
      * @return MongoClient:the MongoDB client which is used to access the MongoDB.
      */
-	public MongoClient getOldShardedPersistClient(long key)
-	{
-		return this.PersistShardingMap.get(((int)(key & 0xFF)) % this.PERSIST_SERVER_NUM_OLD);
-	}
+    public MongoClient getOldShardedPersistClient(long key)
+    {
+	return this.PersistShardingMap.get(((int)(key & 0xFF)) % this.PERSIST_SERVER_NUM_OLD);
+    }
 	
     /**
      * @Title: getNewShardedPersistClient.
@@ -252,21 +248,21 @@ public class PersistentLayerClient
      * @param key: long value which is indicates the value.
      * @return MongoClient:the MongoDB client which is used to access the MongoDB.
      */
-	public MongoClient getNewShardedPersistClient(long key)
-	{
-		return this.PersistShardingMap.get(((int)(key & 0xFF)) % this.PERSIST_SERVER_NUM_NEW);
-	}
+    public MongoClient getNewShardedPersistClient(long key)
+    {
+	return this.PersistShardingMap.get(((int)(key & 0xFF)) % this.PERSIST_SERVER_NUM_NEW);
+    }
 	
-	 /**
+    /**
      * @Title: getOldShardedPersistClient.
      * @Description: the function which is used to fetch the JedisSentinelPool mod by the PERSIST_SERVER_NUM_OLD.
      * @param key: String value which is indicates the value.
      * @return MongoClient:the MongoDB client which is used to access the MongoDB.
      */
-	public MongoClient getOldShardedPersistClient(String key)
-	{
-		return this.PersistShardingMap.get((int)(key.toCharArray()[key.length()-1]) % this.PERSIST_SERVER_NUM_OLD);
-	}
+    public MongoClient getOldShardedPersistClient(String key)
+    {
+	return this.PersistShardingMap.get((int)(key.toCharArray()[key.length()-1]) % this.PERSIST_SERVER_NUM_OLD);
+    }
 	
     /**
      * @Title: getNewShardedPersistClient.
@@ -274,10 +270,10 @@ public class PersistentLayerClient
      * @param key: String value which is indicates the value.
      * @return MongoClient:the MongoDB client which is used to access the MongoDB.
      */
-	public MongoClient getNewShardedPersistClient(String key)
-	{
-		return this.PersistShardingMap.get((int)(key.toCharArray()[key.length()-1]) % this.PERSIST_SERVER_NUM_NEW);
-	}
+    public MongoClient getNewShardedPersistClient(String key)
+    {
+	return this.PersistShardingMap.get((int)(key.toCharArray()[key.length()-1]) % this.PERSIST_SERVER_NUM_NEW);
+    }
 	
     /**
      * @Title: PersistentLayerClient.
@@ -288,9 +284,9 @@ public class PersistentLayerClient
      * @param channel: the channel that subscribed by the PersistentLayerClient.
      * @return none.
      */
-	public PersistentLayerClient(String serverinfo_0, String serverinfo_1, String serverinfo_2, String channel)
-	{
-		this.mongoOption = new MongoClientOptions.Builder().socketKeepAlive(true)
+    public PersistentLayerClient(String serverinfo_0, String serverinfo_1, String serverinfo_2, String channel)
+    {
+	this.mongoOption = new MongoClientOptions.Builder().socketKeepAlive(true)
 				           .connectTimeout(50000)
 				           .socketTimeout(30000)
 				           .readPreference(ReadPreference.primary())
@@ -298,8 +294,8 @@ public class PersistentLayerClient
 				           .maxWaitTime(1000*60*2)
 				           .threadsAllowedToBlockForConnectionMultiplier(40)
 				           .writeConcern(WriteConcern.NORMAL).build();
-		this.ConfigDBInit(serverinfo_0, serverinfo_1, serverinfo_2);
-		this.PersistShardingMap = new HashMap<Integer, MongoClient>();
-		this.Subscriber(channel);
-	}
+	this.ConfigDBInit(serverinfo_0, serverinfo_1, serverinfo_2);
+	this.PersistShardingMap = new HashMap<Integer, MongoClient>();
+	this.Subscriber(channel);
+    }
 }
